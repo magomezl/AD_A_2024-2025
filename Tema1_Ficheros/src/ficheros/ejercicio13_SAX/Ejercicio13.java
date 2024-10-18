4dad182f3bb84e371aadeb510c9d0f7b54cfe21d
magomezl <98543790+magomezl@users.noreply.github.com>
2024-10-18 10:56:09 +0200

    Ejercicio 13. Falta Ampliación

---
+++ Tema1_Ficheros/src/ficheros/ejercicio13_SAX/Ejercicio13.java
@@ -0,0 +1,32 @@
+package ficheros.ejercicio13_SAX;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import ficheros.Ejercicio3;
+
+public class Ejercicio13 {
+	private final static String DOCTRABAJO = "Ejercicio13.xml";
+	public static void main(String[] args) {
+		
+		try {
+			SAXParserFactory sPF = SAXParserFactory.newInstance();
+			SAXParser sP = sPF.newSAXParser();
+			sP.parse(new InputSource(Ejercicio3.RUTA+DOCTRABAJO), new Manejador());
+		} catch (ParserConfigurationException | SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
