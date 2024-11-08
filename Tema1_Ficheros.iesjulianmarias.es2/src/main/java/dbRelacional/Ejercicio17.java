dd11690f9da9ef53f2a9ce88980e32a0658b386d
magomezl <98543790+magomezl@users.noreply.github.com>
2024-11-08 10:18:34 +0100

    Ejercicio 17 sin ampliación

--- Tema1_Ficheros.iesjulianmarias.es2/src/main/java/dbRelacional/Ejercicio17.java
+++ Tema1_Ficheros.iesjulianmarias.es2/src/main/java/dbRelacional/Ejercicio17.java
@@ -6,23 +6,80 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import utilidades.Utilidades;
+
 public class Ejercicio17 {
 
 	private static Connection con;
+	private final static String DB = "dbEmpresaSQLite.dat";
 	
 	public Ejercicio17() {
 		
 	}
 
 	public static void main(String[] args) {
-		establecerConexion();
+		establecerConexion_SQLite();
+		System.out.println(anadirDpto("Ventas", "Sevilla")!=0?"El departamento se añadió con éxito": "El departamento NO se añadió");
+		System.out.println(anadirDpto("Fabricación", "Sevilla")!=0?"El departamento se añadió con éxito": "El departamento NO se añadió");
+		
 		listarDptos();
+		System.out.println(borrarDpto(10)!=0?"El departamento se eliminó con éxito": "El departamento NO se ELIMINO");
+		System.out.println(borrarDpto(1)!=0?"El departamento se eliminó con éxito": "El departamento NO se ELIMINO");
+		listarDptos();
+		System.out.println(modificarDpto(2, "RRHH", "Burgos")!=0?"El departamento se modificó con éxito": "El departamento NO se MODIFICO");
+		listarDptos();
+		
+//		listarDptos();
+//		System.out.println(borrarDpto(10)!=0?"El departamento se eliminó con éxito": "El departamento NO se ELIMINO");
+//		System.out.println(borrarDpto(100)!=0?"El departamento se eliminó con éxito": "El departamento NO se ELIMINO");
+//		listarDptos();
+//		System.out.println(modificarDpto(20, "RRHH", "Burgos")!=0?"El departamento se modificó con éxito": "El departamento NO se MODIFICO");
+//		System.out.println(modificarDpto(200, "RRHH", "Burgos")!=0?"El departamento se modificó con éxito": "El departamento NO se MODIFICO");
+//		listarDptos();
 		cerrarConexion();
 	}
 
+	private static int anadirDpto(String dptoNom, String dptoLoc) {
+		try {
+			PreparedStatement sentencia = con.prepareStatement("INSERT INTO departamentos (dnombre, loc) VALUES (?, ?)");
+			sentencia.setString(1, dptoNom);
+			sentencia.setString(2, dptoLoc);
+			return sentencia.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	
+	private static int borrarDpto(Integer dptoNum) {
+		try {
+			PreparedStatement sentencia = con.prepareStatement("DELETE FROM departamentos WHERE (dept_no = ?)");
+			sentencia.setInt(1, dptoNum);
+			return sentencia.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	private static int modificarDpto(Integer dptoNum, String dptoNom, String dptoLoc) {
+		try {
+			PreparedStatement sentencia = con.prepareStatement("UPDATE departamentos SET dnombre = ?, loc = ? WHERE dept_no = ?");
+			sentencia.setString(1, dptoNom);
+			sentencia.setString(2, dptoLoc);
+			sentencia.setInt(3, dptoNum);
+			return sentencia.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	
 	private static void listarDptos() {
 		try {
-			PreparedStatement sentencia = con.prepareStatement("SELECT * FROM empresa.departamentos;");
+			PreparedStatement sentencia = con.prepareStatement("SELECT * FROM departamentos");
 			ResultSet rS = sentencia.executeQuery();
 			while(rS.next()) {
 				System.out.println(rS.getInt(1) + " " + rS.getString(2) + " (" + rS.getString(3) + ")");
@@ -41,7 +98,7 @@ public class Ejercicio17 {
 		}
 	}
 
-	private static void establecerConexion() {
+	private static void establecerConexion_MySQL() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/empresa", "aurora", "aurora");
@@ -50,7 +107,20 @@ public class Ejercicio17 {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+	}
+	
+	private static void establecerConexion_SQLite() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection("jdbc:sqlite:"+Utilidades.RUTA+DB);
+			PreparedStatement sentencia = con.prepareStatement(
+					"CREATE TABLE IF NOT EXISTS departamentos (dept_no INTEGER PRIMARY KEY AUTOINCREMENT , dnombre VARCHAR(15), loc VARCHAR(15));" );
+			sentencia.executeUpdate();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
